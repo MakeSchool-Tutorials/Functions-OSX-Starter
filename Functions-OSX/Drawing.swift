40b6a0900092c08aed17d8d2633452c068690496
Dion Larson <me@dionlarson.com>
2018-02-26 14:33:30 -0500

    Update for Swift 4

--- Functions-OSX/Drawing.swift
+++ Functions-OSX/Drawing.swift
@@ -10,8 +10,21 @@ import Foundation
 
 func runDrawing() {
     // drawing code goes below here
-    
-    
+    Pen.delay = 0
+    for i in 0...10000 {
+        switch i % 3 {
+        case 0:
+            setColor(red: 1, green: 0, blue: 0)
+        case 1:
+            setColor(red: 0, green: 1, blue: 0)
+        case 2:
+            setColor(red: 0, green: 0, blue: 1)
+        default:
+            continue
+        }
+        move(steps: i)
+        rotate(degrees: 121)
+    }
     
     // drawing code goes above here
 }
